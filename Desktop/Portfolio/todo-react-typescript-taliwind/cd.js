a3118818796ee71990ae4aacd79f43bb34ac7257
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-01-10 18:19:52 -0500

    todo app with react typescript tailwind

--- Desktop/Portfolio/todo-react-typescript-taliwind/cd.js
+++
@@ -1,9 +0,0 @@
-
-var type = new Typed('.typing-text',{
-    strings : ['Web Designer', 'Front End Developer', 'Graphics Designer', 'Photographer'],
-    typeSpeed : 120,
-    loop:true
- });
- const changeBack = document.querySelector("cahnge-back");
-
- changeBack.addEventListener('click')
---
+++ Desktop/Portfolio/todo-react-typescript-taliwind/client/src/App.tsx
@@ -0,0 +1,11 @@
+import Todos  from "./components/Todos";
+
+const App = () => {
+  return (
+    <div className="App h-screen flex justify-center bg-gray-100" >
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
