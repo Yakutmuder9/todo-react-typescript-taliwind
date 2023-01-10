9047fcd19b64b5818359a11b617a0307f7e1afe5
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-01-10 15:58:27 -0500

    initial status

---
+++ Desktop/Portfolio/todo-react-typescript-taliwind/cd.js
@@ -0,0 +1,9 @@
+
+var type = new Typed('.typing-text',{
+    strings : ['Web Designer', 'Front End Developer', 'Graphics Designer', 'Photographer'],
+    typeSpeed : 120,
+    loop:true
+ });
+ const changeBack = document.querySelector("cahnge-back");
+
+ changeBack.addEventListener('click')
